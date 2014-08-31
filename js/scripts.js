33f92f48b3d71073710273c48c371aa1394c9ab4
Dan Sheard <dan.c.sheard@gmail.com>
2014-08-31 21:42:04 +0100

    Styling calculator and refactoring javascript

--- js/scripts.js
+++ js/scripts.js
@@ -2,53 +2,73 @@
 var calculator = (function() {
 
 	var add = document.getElementById("btnAdd"),
-	subtract = document.getElementById("btnSubtract");
+	subtract = document.getElementById("btnSubtract"),
+	elemArray = [],
+	result,
+	answer,
+	operator;
 
-	add.onclick = function adding(e) {
-		
-		document.getElementById("errors").style.display = "none";
-		e.preventDefault();
+	add.onclick = function adding(e) {		
 		var xVal = parseInt(document.getElementById("xValueField").value, 10),
 		yVal = parseInt(document.getElementById("yValueField").value, 10),
-			result, answer;
+		operator = '+';
 
-		if (isNaN(xVal) || isNaN(yVal)) {
-			document.getElementById("errors").style.display = "block";
-		} else {
-			answer = xVal + yVal;
-			result = xVal + " + " + yVal + " = " + answer;
-			document.getElementById("result").innerHTML = result;
-			return result
+		e.preventDefault();
+		validateInput(elemArray);
+
+		answer = xVal + yVal;
+		result = xVal + ' ' + operator + ' ' + yVal + ' ' + " = " + ' ' + answer;
+		document.getElementById("result").innerHTML = result;
+	}
+
+	subtract.onclick = function subtracting(e) {
+		var xVal = parseInt(document.getElementById("xValueField").value, 10),
+		yVal = parseInt(document.getElementById("yValueField").value, 10),
+		operator = '-';
+
+		e.preventDefault();
+		validateInput(elemArray);
+
+		answer = xVal - yVal;
+		result = xVal + ' ' + operator + ' ' + yVal + ' ' + " = " + ' ' + answer;
+		document.getElementById("result").innerHTML = result;
+	}
+
+
+	var validateInput = function validateInput(validateArray) {
+		for (var i = 0; i < validateArray.length; i++) {
+
+			validateArray[i].style.border = "1px solid black";
+			document.getElementById("errors").style.display = "none";
+
+			if (isNaN(validateArray[i].value) || validateArray[i].value === "") {
+				document.getElementById("errors").style.display = "block";
+				validateArray[i].style.border = "1px solid red";
+			}
+
 		}
 	}
 
-	subtract.onclick = function adding(e) {
-		
-		document.getElementById("errors").style.display = "none";
-		e.preventDefault();
-		var xVal = parseInt(document.getElementById("xValueField").value, 10),
-		yVal = parseInt(document.getElementById("yValueField").value, 10),
-			result, answer;
 
-		if (isNaN(xVal) || isNaN(yVal)) {
-			document.getElementById("errors").style.display = "block";
-		} else {
-			answer = xVal - yVal;
-			result = xVal + " - " + yVal + " = " + answer;
-			document.getElementById("result").innerHTML = result;
-			return result
+	var setUpForm = function setUpForm(form) {
+		var elem = form.elements;
+
+		for ( var i = 0;i < elem.length; i++ ) {
+			if (elem[i].getAttribute('data-validation')) {
+				elemArray.push(elem[i]);
+			}
 		}
-	}
-
+		return elemArray;
+	};
 
 	return { 
 
-		init : function() {
-			
+		init : function(form) {
+			setUpForm(form);
 		}
 
 	};
 
 }());
 
-calculator.init();
+calculator.init(document.getElementById("calulatorForm"));
